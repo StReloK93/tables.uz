33e19dd0bc89885e267d4a051436f9c5987e8533
Shinobi 3D <strelok0493@gmail.com>
2022-06-02 17:45:42 +0500

    f

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -30,8 +30,8 @@ class MailController extends Controller
         $data['otherpage']['needchair'] = ($data['otherpage']['needchair']) ? 'need' : 'no need';
 
         
-        // \Mail::to('info@ergoseatings.com')->send(new \App\Mail\Mail($data));
-        \Mail::to('strelok0493@gmail.com')->send(new \App\Mail\Mail($data));
+        \Mail::to('info@ergoseatings.com')->send(new \App\Mail\Mail($data));
+        // \Mail::to('strelok0493@gmail.com')->send(new \App\Mail\Mail($data));
         return $data;
     }
 
