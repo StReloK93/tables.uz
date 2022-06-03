7d4d547062ec64dc0ed76ee41d7d8b273ec7e560
Shinobi 3D <strelok0493@gmail.com>
2022-06-03 10:08:33 +0500

    f

--- resources/js/desktop/views/Size.vue
+++ resources/js/desktop/views/Size.vue
@@ -1,26 +1,26 @@
 <template>
    <section>
-      <main v-if="$store.state.params.legType == 1 || $store.state.params.legType == 2 || $store.state.params.legType == 3" class="xl:pb-4">
-         <h3 class="font-bold mb-4 xl:text-xl md:text-md text-gray-600">
+      <main v-if="$store.state.params.legType == 1 || $store.state.params.legType == 2 || $store.state.params.legType == 3" class="xl:pb-2">
+         <h3 class="font-bold mb-3 xl:text-xl md:text-md text-gray-600">
             Size
          </h3>
          <div class="text-md flex flex-wrap text-gray-600">
-            <button @click="events.setSize(0)" :class="{'bg-my text-white': $store.state.sizepage.size == 0 }" class="xl:h-16 md:h-14 xl:text-md font-bold md:text-xs w-1/5 text-center rounded-xl mr-3 mb-4 border">
+            <button @click="events.setSize(0)" :class="{'bg-my text-white': $store.state.sizepage.size == 0 }" class="xl:h-16 md:h-14 xl:text-md font-bold md:text-xs w-1/5 text-center rounded-xl mr-3 mb-2 border">
                Small
             </button>
-            <button @click="events.setSize(1)" :class="{'bg-my text-white': $store.state.sizepage.size == 1 }"  class="xl:h-16 md:h-14 xl:text-md font-bold md:text-xs w-1/5 text-center rounded-xl mr-3 mb-4 border">
+            <button @click="events.setSize(1)" :class="{'bg-my text-white': $store.state.sizepage.size == 1 }"  class="xl:h-16 md:h-14 xl:text-md font-bold md:text-xs w-1/5 text-center rounded-xl mr-3 mb-2 border">
                Large
             </button>
          </div>
       </main>
 
-      <main class="xl:pb-8">
-         <h3 class="font-bold  xl:mb-3 md:mb-3  xl:text-xl md:text-md text-gray-600">
+      <main class="xl:pb-2">
+         <h3 class="font-bold mb-2  xl:text-xl md:text-md text-gray-600">
             Enter your requested size
          </h3>
          <main>
             <div class="w-full flex justify-center" v-if="type">
-               <table class="threetable relative w-full">
+               <table class="threetable relative w-4/6">
                   <tr>
                      <td></td>
                      <td colspan="3" class="text-center">
